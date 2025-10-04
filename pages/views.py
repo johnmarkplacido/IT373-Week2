2b4709626f5b2381805f4cd980266a36922d7159
johnmark <johnmark.placido@evsu.edu.ph>
2025-10-04 15:09:06 +0800

    Created with css

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home(request):
+    context = {
+        'title': 'Home',
+        'features': [
+            'Django',
+            'Templates',
+            'Static Files'
+        ]
+    }
+    return render(request, 'home.html', context)
+
